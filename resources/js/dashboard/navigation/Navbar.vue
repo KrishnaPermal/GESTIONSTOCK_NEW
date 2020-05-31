50e39bb8b3f5c756815ade3421d2cb2556dced35
KrishnaPermal <krishna.tantalidespermalnaiken@gmail.com>
2020-05-31 23:20:00 +0400

    ajout login/logout navbar eske lébon

--- resources/js/dashboard/navigation/Navbar.vue
+++ resources/js/dashboard/navigation/Navbar.vue
@@ -8,7 +8,13 @@
           <h1>CONFIPEI</h1>
       </v-toolbar-title>
       <v-spacer></v-spacer>
+<!--ajout login a revoir-->
+      <v-btn v-if="!ischeck" class="mr-4 deep-orange" to="/login">Log In</v-btn>
+      <v-btn v-if="ischeck" class="mr-4 deep-orange" @click="logout">Log Out</v-btn>
+<!--ajout login a revoir-->
     </v-app-bar>
+
+
 
      <v-divider></v-divider>
 
@@ -95,7 +101,7 @@
 
 <v-divider></v-divider>
 
-         <v-list-item link>
+         <!-- <v-list-item link>
           <v-list-item-icon>
             <v-icon>mdi-shopping</v-icon>
           </v-list-item-icon>
@@ -105,7 +111,7 @@
               <router-link :to="{name:'login'}">Login</router-link>
             </v-list-item-title>
           </v-list-item-content>
-         </v-list-item>
+         </v-list-item> -->
 
 
       </v-list>
@@ -115,16 +121,40 @@
 </template>
 
 
+
+
 <script>
+import { authenticationService } from '../../_services/authentication.service';
+import router from '../routes'
 export default {
   data() {
     return {
+      currentUser:null,
       drawer: null,
       items: [
         { title: "Accueil", icon: "mdi-home" },
         { title: "Dashboard", icon: "mdi-view-dashboard" },
         { title: "Confitures", icon: "mdi-shopping" },
-      ]
+        { title: "Producteurs", icon: "mdi-account" },
+        { title: "Client", icon: "mdi-account" },
+        //{ title: "Login", icon: "mdi-login-variant" },
+      ],
+      computed:{
+        ischeck(){
+          return this.currentUser
+        }
+      },
+      methods:{
+        logout(){
+          authenticationService.logout();
+          router.push('/login')
+        }
+      },
+      created(){
+        authenticationService.currentUser.subscribe(x => (this.currentUser =x));
+      }
+
+      
     };
   }
 };
